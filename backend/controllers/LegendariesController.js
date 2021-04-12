a6b9ee62913d0b622cb97d888ca8fb48faa70b68
Fabiano SantAnna <vsantanna.fabiano@gmail.com>
2021-04-11 23:05:35 -0300

    Desafio aula20

--- backend/controllers/LegendariesController.js
+++ backend/controllers/LegendariesController.js
@@ -1,4 +1,8 @@
+const { EMLINK } = require('constants');
+const fs = require('fs');
+const listLegendaries = require('../legendaries.json')
 const LegendariesService = require('../services/LegendariesService');
+
 
 const controller = {
     index: (req, res) => {
@@ -10,23 +14,63 @@ const controller = {
     },
 
     create: (req, res) => {
-        const {
-               name,  
-               description, 
-               type, 
-               healthPoints, 
-               specialAttack, 
-               defense, 
-               attack, 
-               experience, 
-               specialDefense,
+        fs.readFile('../legendaries.json', 'utf8', (err, jsonLegendaries) => {
+            if (err) throw err;
+            
+            const {
+                name,  
+                description, 
+                type, 
+                healthPoints, 
+                specialAttack, 
+                defense, 
+                attack, 
+                experience, 
+                specialDefense,
             } = req.body;
 
-            if(!description) {
-                return res.status(400).json({error: 'Description is required'})
-            }
+            const legendary = LegendariesService.createLegendary(
+                name,  
+                description, 
+                type, 
+                healthPoints, 
+                specialAttack, 
+                defense, 
+                attack, 
+                experience, 
+                specialDefense,
+            )
+                
+            let arrayLegendaries = JSON.parse(jsonLegendaries);
 
-        const legendary = LegendariesService.createLegendary(
+            arrayLegendaries.push(legendary);           
+            const legendariesList = () => JSON.stringify(arrayLegendaries, null, 2);
+
+            fs.writeFile('../legendaries.json', legendariesList(), err => {
+                if (err) throw err;
+                console.log('Pokemon criado com sucesso !')
+            });
+
+            return res.json(legendary);
+        })
+    }, 
+
+    read: (req, res) => {
+        fs.readFile('../legendaries.json', 'utf8', (err, jsonLegendaries) => {
+            if (err) throw err;
+            const legendariesList = JSON.parse(jsonLegendaries)
+            return res.json(legendariesList);
+        })
+    },
+
+    update: (req, res) => {
+        const { id } = req.params;
+
+        if(!id) {
+            return res.status(400).json({error: 'ID is required'})
+        }
+
+        const {
             name,  
             description, 
             type, 
@@ -36,32 +80,61 @@ const controller = {
             attack, 
             experience, 
             specialDefense,
-        )
+        } = req.body;
 
-        return res.json(legendary);
-    }, 
+        fs.readFile('../legendaries.json', 'utf8', (err, jsonLegendaries) => {
+            if (err) throw err;
+            const arrayLegendaries = JSON.parse(jsonLegendaries);
 
-    update: (req, res) => {
-        const { id } = req.params;
+            arrayLegendaries.foreach(elem => {
+                if(elem.id === id) {
+                    elem.name = name;
+                    elem.description = description;
+                    elem.type = type;
+                    elem.healthPoints = healthPoints; 
+                    elem.specialAttack = specialAttack; 
+                    elem.defense = defense;
+                    elem.attack = attack;
+                    elem.experience = experience;
+                    elem.specialDefense = specialDefense;
+                }
 
-        const {
-            name,  
-            description, 
-            type, 
-            healthPoints, 
-            specialAttack, 
-            defense, 
-            attack, 
-            experience, 
-            specialDefense
-         } = req.body;
+            let legendariesList = () => JSON.stringify(arrayLegendaries, null, 2);
+            fs.writeFile('../legendaries.json', legendariesList(), err =>{
+                if(err) throw err;
+                console.log('Legendary deletado com sucesso')
+            })
+    
+            return res.json(legendariesList());
+            })
+        })        
+    },
 
-         if(!description) {
-             return res.status(400).json({error: 'Description is required'})
-         }
+    delete: (req, res) => {
 
-        return res.json()
+        let { id } = req.params;
+
+        if(!id) {
+            return res.status(400).json({error: 'ID is required'})
+        }
+
+        fs.readFile('../legendaries.json', 'utf8', (err, jsonLegendaries) => {
+            if(err) throw err;
+
+            let arrayLegendaries = JSON.parse(jsonLegendaries);
+            
+            arrayLegendaries.filter(elem => elem.id !== id);
+            const legendariesList = () => JSON.stringify(arrayLegendaries, null, 2)
+            
+            fs.writeFile('../legendaries.json', legendariesList(), err => {
+                if (err) throw err;
+                console.log('Pokemon excluído com sucesso')
+            });
+
+            res.json(legendariesList());
+        });
     }
 }
+
 
 module.exports = controller;
--- backend/routes/legendaries.js
+++ backend/routes/legendaries.js
@@ -5,7 +5,9 @@ const controller = require('../controllers/LegendariesController');
 /* GET legendaries. */
 router.get('/', controller.index);
 router.post('/', controller.create);
+router.get('/listlegendaries', controller.read);
 router.put('/:id', controller.update);
+router.delete('/:id', controller.delete)
 
 
 module.exports = router;
